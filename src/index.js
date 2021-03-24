3f129df298f40be8857a4085cf7bda4399e7d4ef
luvycunanan <luvycunanan@gmail.com>
2021-03-23 23:55:31 -0700

    add comments

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.scss'
-// import App from './App'
+import App from './App'
 import { HashRouter } from 'react-router-dom'
-import Homepage from './components/Homepage/Homepage'
+// import Homepage from './components/Homepage/Homepage'
 
 import 'bootstrap/js/dist/dropdown'
 
 const appJsx = (
   <HashRouter>
-    {/* <App /> */}
-    <Homepage />
+    <App />
+    {/* <Homepage /> */}
   </HashRouter>
 )
 
